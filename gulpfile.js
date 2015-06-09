3d5fe16a13cdd0cc24c58b4ef9595c5e4b5b569d
Caio Gondim <me@caiogondim.com>
2015-06-10 00:07:02 +0200

    Adds build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp')
 var mocha = require('gulp-mocha')
+var uglify = require('gulp-uglify')
+var header = require('gulp-header')
+var rename = require('gulp-rename')
 
 // Test
 // ----
@@ -7,4 +10,27 @@ var mocha = require('gulp-mocha')
 gulp.task('test', function () {
   gulp.src('test/*.js')
     .pipe(mocha())
+})
+
+// Build
+// -----
+
+var pkg = require('./package.json')
+var banner = [
+  '/**',
+  ' * <%= pkg.name %> - <%= pkg.description %>',
+  ' *',
+  ' * @version v<%= pkg.version %>',
+  ' * @link <%= pkg.homepage %>',
+  ' * @author <%= pkg.author %>',
+  ' * @license <%= pkg.license %>',
+  ' */',
+  ''].join('\n');
+
+gulp.task('build', function() {
+  gulp.src('src/index.js')
+    .pipe(uglify())
+    .pipe(header(banner, {pkg: pkg}))
+    .pipe(rename('msleep.min.js'))
+    .pipe(gulp.dest('dist'))
 })
